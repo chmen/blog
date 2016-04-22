4e696c53834087b8b6025e7cef2004c12f4dbb4b
anton <tony.potapov@gmail.com>
2016-04-22 12:48:49 +0300

    add status to article model, controllers and views fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :status)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   has_many :comments
   validates :title, presence: true, length: { minimum: 5}
-
+  validates :status, presence: true, inclusion: { in: (0..5) }
 end
